722bb9bcead5410a646cb149d48b66512c9968b1
andrew <andrew.r224@gmail.com>
2021-10-11 10:54:40 +0300

    Extract general methods into Application controller :tada:

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::API
+  protected
+
+  def json_response(serializer, status: :ok)
+    render json: serializer, status: status
+  end
+
+  def respond_with_errors(errors)
+    render json:   { errors: ErrorSerializer.new(errors).serialized_json },
+           status: :unprocessable_entity
+  end
 end
--- app/controllers/v1/stocks_controller.rb
+++ app/controllers/v1/stocks_controller.rb
@@ -43,15 +43,6 @@ module V1
       stock_params.except(:bearer_name)
     end
 
-    def json_response(serializer, status: :ok)
-      render json: serializer, status: status
-    end
-
-    def respond_with_errors(errors)
-      render json:   { errors: ErrorSerializer.new(errors).serialized_json },
-             status: :unprocessable_entity
-    end
-
     def find_stock
       @stock = Stock.find(params[:id])
     end
